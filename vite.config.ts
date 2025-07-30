a3ceee65636b520c0665d834bfd322a8974029f2
haaum666 <moiraidrone@gmail.com>
2025-07-31 01:07:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // Используем переменную окружения Vercel, чтобы определить базовый путь
+  // Для продакшн-сборки на Vercel, базовый путь должен быть '/'
+  // Для локальной разработки, базовый путь может быть './'
+  base: process.env.VERCEL_ENV === 'production' ? '/' : './',
 })
