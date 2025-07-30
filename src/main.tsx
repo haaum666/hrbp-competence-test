a10f800ca2f39832f098a20c75f7b32bbb8c082a
haaum666 <moiraidrone@gmail.com>
2025-07-31 01:39:07 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx'; // Импорт вашего основного компонента App
-import './index.css'; // Импорт глобальных стилей (если они есть)
+import './styles/index.css'; // Импорт глобальных стилей (если они есть)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
