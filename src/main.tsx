472566d6371e9e38289c4df4382fabaf29ef6a52
haaum666 <moiraidrone@gmail.com>
2025-08-01 01:23:24 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // ИЗМЕНЕНО здесь: убрано .tsx
+import App from './App'; // <-- ИЗМЕНЕНИЕ: убрано .tsx
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
