67c4ac31a167a8cb040e5f857883d9515435dd6f
haaum666 <moiraidrone@gmail.com>
2025-08-31 14:57:34 +0300

    Update Header.tsx

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -116,16 +116,17 @@ const Header: React.FC = () => {
             О проекте
           </Link>
 
-          {/* НОВЫЕ ССЫЛКИ НА ПРОФИЛИ */}
-          <a href="https://t.me/hr_dushnila" target="_blank" rel="noopener noreferrer" className="text-text-primary hover:text-accent-primary transition-colors duration-300">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L6 12zm0 0h12" />
+          {/* КЛАССИЧЕСКАЯ ИКОНКА TELEGRAM */}
+          <a href="https://t.me/hr_dushnila" target="_blank" rel="noopener noreferrer" className="transition-colors duration-300 hover:opacity-75">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="rgb(34,153,221)">
+              <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm4.35 8.16l-2.486 11.23c-.15.676-.562.847-.947.847-.282 0-.374-.117-.557-.291-.322-.387-1.785-1.185-2.296-1.378-.344-.131-.644-.249-.854-.249-.24 0-.458.128-.621.282l-2.036 1.956c-.255.245-.487.472-.82.472-.455 0-.621-.205-.733-.557L4.1 11.458c-.131-.482.029-.861.424-1.015l11.666-4.498c.404-.155.776-.046.993.284.218.33.242.756.069 1.107z"/>
             </svg>
           </a>
 
-          <a href="https://www.linkedin.com/in/hr-dushnila/" target="_blank" rel="noopener noreferrer" className="text-text-primary hover:text-accent-primary transition-colors duration-300">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.765s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.765-1.75 1.765zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+          {/* КЛАССИЧЕСКАЯ ИКОНКА LINKEDIN */}
+          <a href="https://www.linkedin.com/in/hr-dushnila/" target="_blank" rel="noopener noreferrer" className="transition-colors duration-300 hover:opacity-75">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="rgb(10,102,194)">
+              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.765s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.765-1.75 1.765zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" fill="#FFFFFF"/>
             </svg>
           </a>
         </nav>
