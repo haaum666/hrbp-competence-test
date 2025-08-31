7057f705eeb059bd5c40c0deca6887eeeb05c5d5
haaum666 <moiraidrone@gmail.com>
2025-08-31 16:46:11 +0300

    Update Header.tsx

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -116,19 +116,7 @@ const Header: React.FC = () => {
             О проекте
           </Link>
 
-          {/* ИКОНКА TELEGRAM (теперь с цветом) */}
-          <a href="https://t.me/hr_dushnila" target="_blank" rel="noopener noreferrer" className="transition-colors duration-300 hover:opacity-75">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#26A5E4" width="24" height="24">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L6 12zm0 0h12" />
-            </svg>
-          </a>
-
-          {/* ИКОНКА LINKEDIN (теперь с цветом) */}
-          <a href="https://www.linkedin.com/in/hr-dushnila/" target="_blank" rel="noopener noreferrer" className="transition-colors duration-300 hover:opacity-75">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#0A66C2">
-              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.765s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.765-1.75 1.765zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
-            </svg>
-          </a>
+          {/* ИКОНКИ TELEGRAM И LINKEDIN УДАЛЕНЫ */}
         </nav>
       </div>
     </header>
